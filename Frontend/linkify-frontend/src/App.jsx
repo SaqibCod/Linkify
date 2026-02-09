979ae9fef175e19856ecd4eb4b3c7ed0aa07007d
SaqibCod <saqibbca2021@gmail.com>
2026-02-09 06:54:02 +0530

    impl landing Page

--- Frontend/linkify-frontend/src/App.jsx
+++ Frontend/linkify-frontend/src/App.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import About from './components/About'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path='' element={<LandingPage/>}/>
         <Route path='/About' element={<About/>}/>
         <Route />
       </Routes>
+      <Footer />
       </BrowserRouter>
     </>
   )
